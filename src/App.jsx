9062f7d800f90063b967f2b4a761d46e33f8b6b8
unknown <shadanfarrukh1910@gmail.com>
2024-12-22 07:45:09 +0530

    Added NotFound

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import { Route, Routes } from 'react-router-dom';
 import { SiginCard } from '@/components/organisms/Auth/SigninCard';
 import { SignupCard } from '@/components/organisms/Auth/SignupCard';
 import { Auth } from '@/pages/Auth/Auth';
+import { Notfound } from '@/pages/Notfound/Notfound';
+
 
 
 
@@ -14,6 +16,9 @@ function App() {
     <Routes>
       <Route path='/auth/signup' element={< Auth><SignupCard/></Auth>}/>
       <Route path='/auth/signin' element={< Auth><SiginCard/></Auth>}/>
+
+      <Route path='/*' element={<Notfound/>}/>
+
     </Routes>
   );
 }
