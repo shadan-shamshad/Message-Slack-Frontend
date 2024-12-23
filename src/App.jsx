f3df95bab4694a3d2bbd69402587642f25df1aee
unknown <shadanfarrukh1910@gmail.com>
2024-12-23 19:39:26 +0530

    Added Toaster

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,9 @@ import { Route, Routes } from 'react-router-dom';
 import { SigninCard } from '@/components/organisms/Auth/SigninCard';
 import { Auth } from '@/pages/Auth/Auth';
 import { Notfound } from '@/pages/Notfound/Notfound';
-import { SignupContainer } from './components/organisms/Auth/SignupContainer';
+import { SignupContainer } from '@/components/organisms/Auth/SignupContainer';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Toaster } from '@/components/ui/toaster';
 
 
 const queryClient = new QueryClient();
@@ -22,6 +23,7 @@ function App() {
       <Route path='/*' element={<Notfound/>}/>
 
     </Routes>
+    <Toaster />
     </QueryClientProvider>
   );
 }
