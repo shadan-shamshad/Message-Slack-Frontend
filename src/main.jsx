f019a11ef02cec0713ea3e7d848f0d1952124e33
unknown <shadanfarrukh1910@gmail.com>
2025-01-01 22:23:53 +0530

    Installed again ui dialog

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 import App from './App.jsx';
 
+
 createRoot(document.getElementById('root')).render(
         <BrowserRouter>
             <App />
