631ac56352907a363f1af2f678ca3d9857090629
unknown <shadanfarrukh1910@gmail.com>
2025-01-09 15:55:02 +0530

    Added AA item forr toggle toolbar

--- src/components/atoms/Editor/Editor.jsx
+++ src/components/atoms/Editor/Editor.jsx
@@ -1,7 +1,14 @@
 import 'quill/dist/quill.snow.css'; // ES6
 
+import { ImageIcon, XIcon } from 'lucide-react';
 import Quill from 'quill';
 import { useEffect, useRef, useState } from 'react';
+import { MdSend } from 'react-icons/md';
+import { PiTextAa } from 'react-icons/pi';
+
+import { Button } from '@/components/ui/button';
+
+import { Hint } from '../Hint/Hint';
 
 
 export const Editor = ({
@@ -9,15 +16,27 @@ export const Editor = ({
     onSubmit
     // onCancel,
     // placeholder,
-    // disabled,
     // defaultValue
 }) => {
 
     const [isToolbarVisible, setIsToolbarVisible] = useState(false);
 
-    const containerRef = useRef(); // reqd to initialize the editor
+    const [image, setImage] = useState(null);
 
-     useEffect(() => {
+    const containerRef = useRef(); // reqd to initialize the editor
+    const defaultValueRef = useRef();
+    const quillRef = useRef();
+    const imageInputRef = useRef(null);
+
+    function toggleToolbar() {
+        setIsToolbarVisible(!isToolbarVisible);
+        const toolbar = containerRef.current.querySelector('.ql-toolbar');
+        if(toolbar) {
+            toolbar.classList.toggle('hidden');
+        }
+    }
+
+    useEffect(() => {
 
         if(!containerRef.current) return; // if containerRef is not initialized, return
 
@@ -61,21 +80,96 @@ export const Editor = ({
 
         quill.setContents(defaultValueRef.current);
 
-
     }, []);
-      
-      
+
 
     return (
-        <div className='flex flex-col' >
+        <div
+            className='flex flex-col'
+        >
 
-           <div className='flex flex-col border border-slate-300 rounded-md overflow-hidden focus-within:shadow-sm focus-within:border-slate-400 bg-white '>
+            <div
+                className='flex flex-col border border-slate-300 rounded-md overflow-hidden focus-within:shadow-sm focus-within:border-slate-400 bg-white '
+            >
+                <div className='h-full ql-custom' ref={containerRef} />
+                {
+                    image && (
+                        <div
+                            className='p-2'
+                        >
+                            <div className='relative size-[60px] flex items-center justify-center group/image'>
+                                <button
+                                    className='hidden group-hover/image:flex rounded-full bg-black/70 hover:bg-black absolute -top-2.5 -right-2.5 text-white size-6 z-[5] border-2 border-white items-center justify-center'
+                                    onClick={() => {
+                                        setImage(null);
+                                        imageInputRef.current.value = '';
+                                    }}
+                                >
+                                    <XIcon className='size-4' />
+                                </button>
+                                <img 
+                                    src={URL.createObjectURL(image)}
+                                    className='rounded-xl overflow-hidden border object-cover'
+                                />
+                            </div>
+                        </div>
+                    )
+                }
 
-            <div  className='h-full ql-custom' ref={containerRef} />
-                    Editor goes here
+                <div className='flex px-2 pb-2 z-[5]'>
+                    <Hint label={!isToolbarVisible ? 'Show toolbar' : 'Hide toolbar'} side='bottom' align='center'>
+                        <Button
+                            size="iconSm"
+                            variant="ghost"
+                            disabled={false}
+                            onClick={toggleToolbar}
+                        >
+                            <PiTextAa className='size-4' />
+                        </Button>
+                    </Hint>
+
+                    <Hint label="Image">
+                        <Button
+                            size="iconSm"
+                            variant="ghost"
+                            disabled={false}
+                            onClick={() => { imageInputRef.current.click(); }}
+                        >
+                            <ImageIcon className='size-4' />
+                        </Button>
+                    </Hint>
+
+                    <input 
+                        type="file"
+                        className='hidden'
+                        ref={imageInputRef}
+                        onChange={(e) => setImage(e.target.files[0])}
+                    />
+
+                    <Hint label="Send Message">
+                        <Button
+                            size="iconSm"
+                            className="ml-auto bg-[#007a6a] hover:bg-[#007a6a]/80 text-white"
+                            onClick={() => {
+                                const messageContent = JSON.stringify(quillRef.current?.getContents());
+                                onSubmit({ body: messageContent, image });
+                                quillRef.current?.setText('');
+                                setImage(null);
+                                imageInputRef.current.value = '';
+                            }}
+                            disabled={false}
+                        >
+                            <MdSend className='size-4' />
+                        </Button>
+                    </Hint>
+                </div>
             </div>
 
-           </div>
-    
-    )
-}
+            <p
+                className='p-2 text-[10px] text-mutes-foreground flex justify-end'
+            >
+                <strong>Shift + return</strong> &nbsp; to add a new line
+            </p>
+        </div>
+    );
+};
